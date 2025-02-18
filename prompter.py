3581fa460e6cb915a992007575aeff02066718ca
jeffrankine <jeff.rankine@outlook.com>
2025-02-18 13:06:01 -0500

    Reverting to version wtih no OpenAI call

--- prompter.py
+++ prompter.py
@@ -27,41 +27,37 @@ def chatgpt(prompt):
     deployment = AZURE_OPEN_AI_DEPLOYMENT
     subscription_key = AZURE_OPEN_AI_KEY
     api_version = AZURE_API_VERSION
-
-        # Initialize Azure OpenAI Service client with key-based authentication    
-    client = AzureOpenAI(  
-        azure_endpoint=endpoint,  
-        api_key=subscription_key,  
-        api_version=api_version,
-    )
-    
-
-    #Prepare the chat prompt 
-    chat_prompt = [
-        {
-        "role": "system",
-        "content": "You are an AI assistant that helps people find information."
-        },
-        {
-            "role": "user",
-            "content": prompt
-        }
-    ] 
+    # client = AzureOpenAI(  
+    #     azure_endpoint=endpoint,  
+    #     api_key=subscription_key,  
+    #     api_version=api_version
+    # )
+    # Prepare the chat prompt 
+    #chat_prompt = [
+    #    {
+    #    "role": "system",
+    #    "content": "You are an AI assistant that helps people find information."
+    #    },
+    #    {
+    #        "role": "user",
+    #        "content": prompt
+    #    }
+    #] 
 
     # Include speech result if speech is enabled  
-    messages = chat_prompt  
+    #messages = chat_prompt  
     
     # Generate the completion  
-    completion = client.chat.completions.create(  
-        model=deployment,
-        messages=messages,
-        max_tokens=800,  
-        temperature=0.7,  
-        top_p=0.95,  
-        frequency_penalty=0,  
-        presence_penalty=0,
-        stop=None,  
-        stream=False
-    )
+    # completion = client.chat.completions.create(  
+    #     model=deployment,
+    #     messages=messages,
+    #     max_tokens=800,  
+    #     temperature=0.7,  
+    #     top_p=0.95,  
+    #     frequency_penalty=0,  
+    #     presence_penalty=0,
+    #     stop=None,  
+    #     stream=False
+    # )
 
     return "this is a canned response from promopter"
