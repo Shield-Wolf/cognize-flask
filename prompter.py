f07bff9cb2e820c3c149335f58dc09c2e6e038a4
jeffrankine <jeff.rankine@outlook.com>
2025-02-18 12:09:15 -0500

    Imported just os library

--- prompter.py
+++ prompter.py
@@ -1,7 +1,7 @@
 
-import openai
+#import openai
 import os
-from openai import AzureOpenAI 
+#from openai import AzureOpenAI 
 
 def chatgpt(prompt):
     return "This is a canned response from prompter"
