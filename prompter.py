f73a65851a4595a63df12af7cdf6d3cf7fadda61
jeffrankine <jeff.rankine@outlook.com>
2025-02-19 11:18:28 -0500

    Calling OpenAI

--- prompter.py
+++ prompter.py
@@ -55,7 +55,7 @@ def chatgpt(prompt):
 
     # Include speech result if speech is enabled  
     messages = chat_prompt  
-    Generate the completion  
+    # Generate the completion  
     completion = client.chat.completions.create(  
          model=deployment,
          messages=chat_prompt,
