e33b0ccbce1bd3cb9bd6bfc81245528b9d0bf4f5
jeffrankine <jeff.rankine@outlook.com>
2025-03-02 15:03:53 -0500

    went back to old code that was working

--- app.py
+++ app.py
@@ -17,33 +17,12 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'cognize-logo-favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-# @app.route('/question', methods=['POST'])
-# def question():
-#     question = request.form.get('question')
-#     key = request.form.get('key')                               
-#     response = prompter.chatgpt(question)
-#     #response = "This is a canned response"
-#     if question:
-#         print('Request for home page received with question=%s' % question)
-#          # Call Azure OpenAI
-#          # Call Azure OpenAI with values
-#         print('Request key=%s' % os.environ.get("APP_PERMISSION_KEY" ))
-#         if key != os.environ.get("APP_PERMISSION_KEY"):
-#                print('Request for home page received with key redirecting=%s' % key)
-#                return redirect(url_for('index'))
-
-
-#     # Call Azure OpenAI
-#     # Call Azure OpenAI with values
-# ##    response = prompter.chatgpt(question)
-# ##    print('Response from Azure OpenAI=%s' % response)
-    
-##    return render_template('question.html', question = question, key = key, response = response)
-app.route('/question', methods=['POST'])
+@app.route('/question', methods=['POST'])
 def question():
     question = request.form.get('question')
-    key = request.form.get('key')
-
+    key = request.form.get('key')                               
+    response = prompter.chatgpt(question)
+    #response = "This is a canned response"
     if question:
         print('Request for home page received with question=%s' % question)
          # Call Azure OpenAI
@@ -53,14 +32,35 @@ def question():
                print('Request for home page received with key redirecting=%s' % key)
                return redirect(url_for('index'))
 
-        # Call Azure OpenAI
-        # Call Azure OpenAI with values
-        response = prompter.chatgpt(question)
-        print('Response from Azure OpenAI=%s' % response)
-        return render_template('question.html', question = question, key = key, response = response)
-    else:
-        print('Request for hello page received with no question or blank name -- redirecting')
-        return redirect(url_for('index'))
+
+    # Call Azure OpenAI
+    # Call Azure OpenAI with values
+    response = prompter.chatgpt(question)
+    print('Response from Azure OpenAI=%s' % response)
+    
+##    return render_template('question.html', question = question, key = key, response = response)
+# @app.route('/question', methods=['POST'])
+# def question():
+#     question = request.form.get('question')
+#     key = request.form.get('key')
+
+#     if question:
+#         print('Request for home page received with question=%s' % question)
+#          # Call Azure OpenAI
+#          # Call Azure OpenAI with values
+#         print('Request key=%s' % os.environ.get("APP_PERMISSION_KEY" ))
+#         if key != os.environ.get("APP_PERMISSION_KEY"):
+#                print('Request for home page received with key redirecting=%s' % key)
+#                return redirect(url_for('index'))
+
+#         # Call Azure OpenAI
+#         # Call Azure OpenAI with values
+#         response = prompter.chatgpt(question)
+#         print('Response from Azure OpenAI=%s' % response)
+#         return render_template('question.html', question = question, key = key, response = response)
+#     else:
+#         print('Request for hello page received with no question or blank name -- redirecting')
+#         return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
