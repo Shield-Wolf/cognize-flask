3bdfe4c214588aee977754782ce0422c0a9480e6
jeffrankine <jeff.rankine@outlook.com>
2025-03-02 14:53:46 -0500

    Modified app.py to call chatgpt with correct paramaters and prompts

--- app.py
+++ app.py
@@ -35,32 +35,32 @@ def question():
 
     # Call Azure OpenAI
     # Call Azure OpenAI with values
-    response = prompter.chatgpt(question)
-    print('Response from Azure OpenAI=%s' % response)
+##    response = prompter.chatgpt(question)
+##    print('Response from Azure OpenAI=%s' % response)
     
-    return render_template('question.html', question = question, key = key, response = response)
-#@app.route('/question', methods=['POST'])
-# def question():
-#    question = request.form.get('question')
-#    key = request.form.get('key')
+##    return render_template('question.html', question = question, key = key, response = response)
+app.route('/question', methods=['POST'])
+def question():
+    question = request.form.get('question')
+    key = request.form.get('key')
 
-#    if question:
-#        print('Request for home page received with question=%s' % question)
-#         # Call Azure OpenAI
-#         # Call Azure OpenAI with values
-#        print('Request key=%s' % os.environ.get("APP_PERMISSION_KEY" ))
-#        if key != os.environ.get("APP_PERMISSION_KEY"):
-#               print('Request for home page received with key redirecting=%s' % key)
-#               return redirect(url_for('index'))
+    if question:
+        print('Request for home page received with question=%s' % question)
+         # Call Azure OpenAI
+         # Call Azure OpenAI with values
+        print('Request key=%s' % os.environ.get("APP_PERMISSION_KEY" ))
+        if key != os.environ.get("APP_PERMISSION_KEY"):
+               print('Request for home page received with key redirecting=%s' % key)
+               return redirect(url_for('index'))
 
-#        # Call Azure OpenAI
-#        # Call Azure OpenAI with values
-#        response = prompter_basic.chatgpt(question)
-#        print('Response from Azure OpenAI=%s' % response)
-#        return render_template('question.html', question = question, key = key, response = response)
-#    else:
-#        print('Request for hello page received with no question or blank name -- redirecting')
-#        return redirect(url_for('index'))
+        # Call Azure OpenAI
+        # Call Azure OpenAI with values
+        response = prompter.chatgpt(question)
+        print('Response from Azure OpenAI=%s' % response)
+        return render_template('question.html', question = question, key = key, response = response)
+    else:
+        print('Request for hello page received with no question or blank name -- redirecting')
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
